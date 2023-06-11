07d9e668bb6d9f38e4f2eef211c3bae8e02a25da
phuong23401 <hoaphuong23401@gmail.com>
2023-06-11 23:28:37 +0700

    update section about.

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -13,11 +13,13 @@ import {
   SectionWrapper,
   ContentDesktop,
 } from "./Styled";
+import Button from "components/Button";
 import ETH from "assets/images/About/eth.svg";
 import Coingecko from "assets/images/About/coingecko.svg";
 import CMC from "assets/images/About/cmc.svg";
+import SwipperPrev from "assets/images/swiper_prev.png";
+import SwipperNext from "assets/images/swiper_next.png";
 import { RiShareBoxFill } from "react-icons/ri";
-import Button from "components/Button";
 
 function About(): JSX.Element {
   return (
@@ -32,77 +34,6 @@ function About(): JSX.Element {
             holders of 1,356
           </p>
         </Introduce>
-        <ContentMobile>
-          <Swiper
-            cssMode={true}
-            navigation={true}
-            pagination={true}
-            mousewheel={true}
-            keyboard={true}
-            slidesPerView={1}
-            spaceBetween={20}
-            loop={true}
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-            speed={3000}
-            breakpoints={{
-              576: {
-                slidesPerView: 2,
-                spaceBetween: 30,
-              },
-              992: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-              },
-            }}
-            modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-          >
-            <SwiperSlide>
-              <ContentInner>
-                <img src={ETH} alt="" />
-                <ButtonWrapper>
-                  <Button type="secondary">
-                    Etherscan (ETH)
-                    <RiShareBoxFill size={16} />
-                  </Button>
-                </ButtonWrapper>
-                <ContentText>Marvin ETH contract addresses</ContentText>
-                <p>Transactions</p>
-                <h5>200k+on DEX</h5>
-              </ContentInner>
-            </SwiperSlide>
-            <SwiperSlide>
-              <ContentInner>
-                <img src={Coingecko} alt="" />
-                <ButtonWrapper>
-                  <Button type="secondary">
-                    Verified supply on CoinGecko
-                    <RiShareBoxFill size={16} />
-                  </Button>
-                </ButtonWrapper>
-                <p>Circulating Supply</p>
-                <h5>1,000,000,000,000</h5>
-              </ContentInner>
-            </SwiperSlide>
-            <SwiperSlide>
-              <ContentInner>
-                <img src={CMC} alt="" />
-                <ButtonWrapper>
-                  <Button type="secondary">
-                    Verified supply on CoinMarketCap
-                    <RiShareBoxFill size={16} />
-                  </Button>
-                </ButtonWrapper>
-                <ContentText>Tokenomics</ContentText>
-                <ContentSubText>
-                  Total Supply: 1,000,000,000,000 <br /> 3% buy/sell tax on DEX
-                </ContentSubText>
-              </ContentInner>
-            </SwiperSlide>
-          </Swiper>
-        </ContentMobile>
 
         <div className="d-none d-lg-block">
           <ContentDesktop>
@@ -160,6 +91,83 @@ function About(): JSX.Element {
           </ButtonWrapper>
         </BottomWrapper>
       </Container>
+
+      <ContentMobile>
+        <Swiper
+          cssMode={true}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          pagination={true}
+          mousewheel={true}
+          keyboard={true}
+          slidesPerView={1}
+          spaceBetween={20}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          speed={3000}
+          breakpoints={{
+            576: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            992: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+          }}
+          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        >
+          <SwiperSlide>
+            <ContentInner>
+              <img src={ETH} alt="" />
+              <ButtonWrapper>
+                <Button type="secondary">
+                  Etherscan (ETH)
+                  <RiShareBoxFill size={16} />
+                </Button>
+              </ButtonWrapper>
+              <ContentText>Marvin ETH contract addresses</ContentText>
+              <p>Transactions</p>
+              <h5>200k+on DEX</h5>
+            </ContentInner>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ContentInner>
+              <img src={Coingecko} alt="" />
+              <ButtonWrapper>
+                <Button type="secondary">
+                  Verified supply on CoinGecko
+                  <RiShareBoxFill size={16} />
+                </Button>
+              </ButtonWrapper>
+              <p>Circulating Supply</p>
+              <h5>1,000,000,000,000</h5>
+            </ContentInner>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ContentInner>
+              <img src={CMC} alt="" />
+              <ButtonWrapper>
+                <Button type="secondary">
+                  Verified supply on CoinMarketCap
+                  <RiShareBoxFill size={16} />
+                </Button>
+              </ButtonWrapper>
+              <ContentText>Tokenomics</ContentText>
+              <ContentSubText>
+                Total Supply: 1,000,000,000,000 <br /> 3% buy/sell tax on DEX
+              </ContentSubText>
+            </ContentInner>
+          </SwiperSlide>
+          <img className="swiper-button-prev" src={SwipperPrev} alt="" />
+          <img className="swiper-button-next" src={SwipperNext} alt="" />
+        </Swiper>
+      </ContentMobile>
     </SectionWrapper>
   );
 }
