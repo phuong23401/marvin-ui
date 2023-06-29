d72d48e6ca5b7a313f4e47ff0c9526b63f29fdb9
phuong23401 <hoaphuong23401@gmail.com>
2023-06-29 19:32:52 +0700

    update section community.

--- src/pages/Community/index.tsx
+++ src/pages/Community/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Scroll from "react-scroll";
+import axios from "axios";
 import { Container } from "react-bootstrap";
 import SectionTitle from "components/SectionTitle";
 import {
@@ -14,8 +16,50 @@ import Discord from "assets/images/Community/discord.svg";
 import Twitter from "assets/images/Community/twitter.svg";
 import Youtube from "assets/images/Community/youtube.svg";
 import { RiShareBoxFill } from "react-icons/ri";
+import Loader from "components/Loader";
+
+const apiUrl = "http://127.0.0.1:3005/api";
 
 function Community(): JSX.Element {
+  const [telegramMembers, setTelegramMembers] = useState<any>();
+  const [twitterFollowers, setTwitterFollowers] = useState<any>();
+  const [discordMembers, setDiscordMembers] = useState<any>();
+  const [youtubeSubscribers, setYoutubeSubscribers] = useState<any>();
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/getDataCount`);
+
+      if (response.status === 200 && response.data) {
+        const telegram = response?.data?.find(
+          (data: any) => data.id === "telegram"
+        );
+        const twitter = response?.data?.find(
+          (data: any) => data.id === "twitter"
+        );
+        const discord = response?.data?.find(
+          (data: any) => data.id === "discord"
+        );
+        const youtube = response?.data?.find(
+          (data: any) => data.id === "youtube"
+        );
+
+        if (telegram && twitter && discord && youtube) {
+          setTelegramMembers(telegram.value);
+          setTwitterFollowers(twitter.value ?? 60371);
+          setDiscordMembers(discord.value ?? 808);
+          setYoutubeSubscribers(youtube.value);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  getData();
+  // setInterval(() => {
+  // }, 43200000);
+
   return (
     <Scroll.Element name="community">
       <SectionWrapper>
@@ -25,59 +69,67 @@ function Community(): JSX.Element {
           </SectionTitle>
           <ContentWrapper>
             <ContentInner>
-              <ChannelLogo src={Telegram} alt="" />
-              <ChannelName>
-                Telegram
-                <RiShareBoxFill size={14} />
-              </ChannelName>
-              <ChannelContent>
-                Join the bossiest Web3 community and{" "}
-                <br className="d-none d-xl-block" />
-                get 24/7 support
-              </ChannelContent>
-              <h5>160000</h5>
-              <span>Members</span>
+              <a href="https://www.t.me/marvininuofficial" target="_blank">
+                <ChannelLogo src={Telegram} alt="" />
+                <ChannelName>
+                  Telegram
+                  <RiShareBoxFill size={14} />
+                </ChannelName>
+                <ChannelContent>
+                  Join the bossiest Web3 community and{" "}
+                  <br className="d-none d-xl-block" />
+                  get 24/7 support
+                </ChannelContent>
+                <h5>{telegramMembers ?? <Loader />}</h5>
+                <span>Members</span>
+              </a>
             </ContentInner>
             <ContentInner>
-              <ChannelLogo src={Discord} alt="" />
-              <ChannelName>
-                Discord
-                <RiShareBoxFill size={14} />
-              </ChannelName>
-              <ChannelContent>
-                Join the official Marvin NFT
-                <br className="d-none d-lg-block" />
-                discord
-              </ChannelContent>
-              <h5>837</h5>
-              <span>Members</span>
+              <a href="https://discord.com/invite/KXtBnvh3tE" target="_blank">
+                <ChannelLogo src={Discord} alt="" />
+                <ChannelName>
+                  Discord
+                  <RiShareBoxFill size={14} />
+                </ChannelName>
+                <ChannelContent>
+                  Join the official Marvin NFT
+                  <br className="d-none d-lg-block" />
+                  discord
+                </ChannelContent>
+                <h5>{discordMembers ?? <Loader />}</h5>
+                <span>Members</span>
+              </a>
             </ContentInner>
             <ContentInner>
-              <ChannelLogo src={Twitter} alt="" />
-              <ChannelName>
-                Twitter
-                <RiShareBoxFill size={14} />
-              </ChannelName>
-              <ChannelContent>
-                Follow Marvin and spread the Mars
-                <br className="d-none d-lg-block" />
-                Mission
-              </ChannelContent>
-              <h5>61000</h5>
-              <span>Followers</span>
+              <a href="https://twitter.com/marvin_inu" target="_blank">
+                <ChannelLogo src={Twitter} alt="" />
+                <ChannelName>
+                  Twitter
+                  <RiShareBoxFill size={14} />
+                </ChannelName>
+                <ChannelContent>
+                  Follow Marvin and spread the Mars
+                  <br className="d-none d-lg-block" />
+                  Mission
+                </ChannelContent>
+                <h5>{twitterFollowers ?? <Loader />}</h5>
+                <span>Followers</span>
+              </a>
             </ContentInner>
             <ContentInner>
-              <ChannelLogo src={Youtube} alt="" />
-              <ChannelName>
-                Youtube
-                <RiShareBoxFill size={14} />
-              </ChannelName>
-              <ChannelContent>
-                Education, gameplay videos
-                <br className="d-none d-lg-block" />& more!
-              </ChannelContent>
-              <h5>319</h5>
-              <span>Subscribers</span>
+              <a href="https://www.youtube.com/marvininu" target="_blank">
+                <ChannelLogo src={Youtube} alt="" />
+                <ChannelName>
+                  Youtube
+                  <RiShareBoxFill size={14} />
+                </ChannelName>
+                <ChannelContent>
+                  Education, gameplay videos
+                  <br className="d-none d-lg-block" />& more!
+                </ChannelContent>
+                <h5>{youtubeSubscribers ?? <Loader />}</h5>
+                <span>Subscribers</span>
+              </a>
             </ContentInner>
           </ContentWrapper>
         </Container>
